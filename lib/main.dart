d1cad2a9f6443dd04a57a1bfb7bd98241af730b0
NguyenHoangDuc <66166011+ducsinhvientinhnguyen@users.noreply.github.com>
2022-03-06 16:12:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/camera_screen.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error in fetching the cameras: $e');
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CameraScreen(),
+    );
+  }
+}
